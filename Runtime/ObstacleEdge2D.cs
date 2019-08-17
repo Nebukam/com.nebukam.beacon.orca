c96d6766fc940cdc0b6a392aaf1811e796db8de3
Tim <nebukam@gmail.com>
2019-08-17 02:55:36 -0400

    Update ObstacleEdge2D.cs

--- Runtime/ObstacleEdge2D.cs
+++ Runtime/ObstacleEdge2D.cs
@@ -29,11 +29,15 @@ namespace Nebukam.Beacon.ORCA
     [AddComponentMenu("Nebukam/Beacon/ORCA/Obstacle Edge 2D")]
     public class ObstacleEdge2D : ObstacleConverter<EdgeCollider2D>
     {
+        
+        [Header("Edge")]
+        [Tooltip("[EXPERIMENTAL] Double-sided edge collides from both sides.")]
+        public bool doubleSided = false;
 
         protected override void BuildObstacles()
         {
 
-            SetObstacleCount(1);
+            SetObstacleCount(doubleSided ? 2 : 1);
 
             Vector2[] points = colliderComponent.points;
             Vector2 offset = colliderComponent.offset;
@@ -53,6 +57,15 @@ namespace Nebukam.Beacon.ORCA
 
             for (int i = 0; i < count; i++)
                 o[i].pos = Project(points[i]);
+
+            if (doubleSided)
+            {
+                float3 overlap = float3(float.Epsilon, float.Epsilon, float.Epsilon);
+                o = SetObstacleLength(1, count);
+                o.edge = true;
+                for (int i = 0; i < count; i++)
+                    o[i].pos = Project(points[count-(i+1)]) + overlap;
+            }
 
         }
 
@@ -82,6 +95,13 @@ namespace Nebukam.Beacon.ORCA
 
             for (int i = 1, count = points.Length; i < count; i++)
                 DrawSegment(Project(points[i - 1]), Project(points[i]), col);
+
+            if(doubleSided)
+            {
+                for (int i = 1, count = points.Length; i < count; i++)
+                    DrawSegment(Project(points[count - i]), Project(points[count - (i+1)]), col);
+            }
+
         }
     
         #endregion
